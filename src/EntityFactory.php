e25c90d42c2e3ea6cd816cff59c09eb9acc5a692
Vladimír Bártek <vladimir.bartek@gmail.com>
2024-07-15 17:10:23 +0200

    phpstan level 4

--- src/EntityFactory.php
+++ src/EntityFactory.php
@@ -4,21 +4,16 @@ declare(strict_types=1);
 
 namespace Doomy\Repository;
 
-use Doomy\CustomDibi\Connection;
-use Doomy\Repository\Helper\DbHelper;
-
 readonly class EntityFactory
 {
     public function __construct(
-        private Connection $connection,
         private RepoFactory $repoFactory,
-        private DbHelper $dbHelper
     ) {
     }
 
     public function createEntity($entityClass, $values)
     {
-        // TODO could this somehow be injected instead of creating new instances?
-        return new $entityClass($values, new RepoFactory($this->connection, $this, $this->dbHelper));
+        // TODO: avoid sending repofactory to entity instances
+        return new $entityClass($values, $this->repoFactory);
     }
 }
